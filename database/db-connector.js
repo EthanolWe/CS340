9bd71b3d718f4612f1fe5c6ee07158186d7ba221
EthanolWe <91490612+EthanolWe@users.noreply.github.com>
2022-11-19 15:01:53 -0800

    Security Update

--- database/db-connector.js
+++ database/db-connector.js
@@ -8,7 +8,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
     user            : 'cs340_wentwore',
-    password        : '0033',
+    password        : '',
     database        : 'cs340_wentwore'
 })
 
