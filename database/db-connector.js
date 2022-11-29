18f77383cd22223bd9e8c7d0509b51f075ccd2f2
EthanolWe <91490612+EthanolWe@users.noreply.github.com>
2022-11-28 21:37:41 -0800

    Update db-connector.js

--- database/db-connector.js
+++ database/db-connector.js
@@ -7,7 +7,7 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_wentwore',
+    user            : '',
     password        : '',
     database        : 'cs340_wentwore'
 })
